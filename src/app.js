d696326890e90dd720dcee6323cc833a4a8e1c0c
Alex Riascos <160887203+X4Nn3R@users.noreply.github.com>
2024-07-09 03:27:54 +0000

    add ExcuseGenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ]; //Create a function that returns a vector element randomly
+  function randomMessage(lista) {
+    // Generate a random number
+    let randomNumber = Math.floor(Math.random() * lista.length);
+    return lista[randomNumber] + " ";
+  }
+
+  // Call the function and save the result in a variable
+  var randomWord =
+    randomMessage(who) +
+    randomMessage(action) +
+    randomMessage(what) +
+    randomMessage(when);
+  // Show the message when refreshing the page
+  document.querySelector("#excuse").innerHTML = randomWord;
+
+  // Show the message when you click the button
+  document.getElementById("btn").addEventListener("click", () => {
+    var randomWord =
+      randomMessage(who) +
+      randomMessage(action) +
+      randomMessage(what) +
+      randomMessage(when);
+    document.getElementById("excuse").innerHTML = randomWord;
+  });
 };
